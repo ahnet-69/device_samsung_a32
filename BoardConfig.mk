20189bc04014aac28881098657f189209c94eb13
ahnet-69 <syedmusicrealise@gmail.com>
2023-10-20 15:16:45 +0500

    a32: Include evolution-X config and sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,7 @@ VENDOR_SECURITY_PATCH := 2023-08-01
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+include device/evolution/sepolicy/common/sepolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
