03909867c7163d464425782cfd8c0aa83783ee5c
ahnet-69 <syedmusicrealise@gmail.com>
2023-10-22 21:08:08 +0500

    releasetools: Fixup of 01aeda297d7457e6cda8b88925f03981d264ae3b
    Where did the duplicated boot.img even come from

--- releasetools.py
+++ releasetools.py
@@ -27,5 +27,5 @@ def OTA_InstallEnd(info):
   AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
   AddImage(info, "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor")
   AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
-  AddImage(info, "boot.img", "boot.img", "/dev/block/by-name/boot", zip=False)
+  AddImage(info, "boot.img", "/dev/block/by-name/boot", zip=False)
   return
